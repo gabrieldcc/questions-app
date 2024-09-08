8fb9bde5dae925d89712bc58e61c4affe11bed7b
Gabriel Castro <gabrieldcc@gmail.com>
2024-09-08 17:55:42 -0300

    default state to radio

--- App.tsx
+++ App.tsx
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { Box, Text, FlatList, Heading, VStack, NativeBaseProvider } from 'native-base';
+import { fetchQuestions } from './src/services/questions-service';
+import CardQuestion from './src/components/CardQuestion';
+
+interface Question {
+  _id: string;
+  question: string;
+  options: string[];
+  answer: string;
+}
 
 export default function App() {
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  async function getQuestions() {
+    try {
+      const data = await fetchQuestions();
+      console.log('setting questions', data);
+      setQuestions(data);
+    } catch (error) {
+      console.error('Erro ao carregar as questões:', error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+
+    <SafeAreaView style={styles.container}>
+      <VStack space={4} alignItems="center" mb={16}>
+        <Heading mb={4}>Questões</Heading>
+        <FlatList
+          data={questions}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => (
+            < CardQuestion 
+              answer={item.answer}
+              question={item.question} 
+              options={item.options}            
+              />
+          )}
+        />
+      </VStack>
+    </SafeAreaView>
+    </NativeBaseProvider>
+
   );
 }
 
@@ -14,7 +56,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
