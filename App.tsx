771d1bde109ea30f1fe9c736f711070644c48ad7
Gabriel Castro <gabrieldcc@gmail.com>
2024-09-11 22:37:19 -0300

    change to react native components to render in webview

--- App.tsx
+++ App.tsx
@@ -1,6 +1,68 @@
+// import React, { useEffect, useState } from 'react';
+// import { StyleSheet, SafeAreaView } from 'react-native';
+// import { Box, Text, FlatList, Heading, VStack, NativeBaseProvider } from 'native-base';
+// import { fetchQuestions } from './src/services/questions-service';
+// import CardQuestion from './src/components/CardQuestion';
+
+// interface Question {
+//   _id: string;
+//   question: string;
+//   options: string[];
+//   answer: string;
+// }
+
+// export default function App() {
+//   const [questions, setQuestions] = useState<Question[]>([]);
+
+//   useEffect(() => {
+//     getQuestions();
+//   }, []);
+
+//   async function getQuestions() {
+//     try {
+//       const data = await fetchQuestions();
+//       console.log('setting questions', data);
+//       setQuestions(data);
+//     } catch (error) {
+//       console.error('Erro ao carregar as questões:', error);
+//     }
+//   }
+
+//   return (
+//     <NativeBaseProvider>
+
+//     <SafeAreaView style={styles.container}>
+//       <VStack space={4} alignItems="center" mb={16}>
+//         <Heading mb={4}>Questões</Heading>
+//         <FlatList
+//           data={questions}
+//           keyExtractor={(item) => item._id}
+//           renderItem={({ item }) => (
+//             < CardQuestion 
+//               answer={item.answer}
+//               question={item.question} 
+//               options={item.options}            
+//               />
+//           )}
+//         />
+//       </VStack>
+//     </SafeAreaView>
+//     </NativeBaseProvider>
+
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     padding: 20,
+//   },
+// });
+
+
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { Box, Text, FlatList, Heading, VStack, NativeBaseProvider } from 'native-base';
+import { StyleSheet, SafeAreaView, View, Text, FlatList, ScrollView } from 'react-native';
 import { fetchQuestions } from './src/services/questions-service';
 import CardQuestion from './src/components/CardQuestion';
 
@@ -29,26 +91,24 @@ export default function App() {
   }
 
   return (
-    <NativeBaseProvider>
-
     <SafeAreaView style={styles.container}>
-      <VStack space={4} alignItems="center" mb={16}>
-        <Heading mb={4}>Questões</Heading>
-        <FlatList
-          data={questions}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => (
-            < CardQuestion 
-              answer={item.answer}
-              question={item.question} 
-              options={item.options}            
-              />
-          )}
-        />
-      </VStack>
+      <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.heading}>Questões</Text>
+      </View>
+      <FlatList
+        data={questions}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <CardQuestion
+            answer={item.answer}
+            question={item.question}
+            options={item.options}
+          />
+        )}
+      />
+      </ScrollView>
     </SafeAreaView>
-    </NativeBaseProvider>
-
   );
 }
 
@@ -57,5 +117,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     padding: 20,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
